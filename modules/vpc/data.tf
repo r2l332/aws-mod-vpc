72680fea599d92f2cdc4d2a7ed9c15a6fe5b6416
lee.jelley@contino.io <ljelley@lees-mbp.home>
2020-08-10 09:21:41 +0100

    Added changes to VPC creation

---
+++ modules/vpc/data.tf
@@ -0,0 +1,7 @@
+data "aws_availability_zones" "available_azs" {
+  state = "available"
+  filter {
+      name  = "region-name"
+      values = [var.region]
+  }
+}
--- modules/vpc/locals.tf
+++ modules/vpc/locals.tf
@@ -11,5 +11,14 @@ locals {
     name    = "eks.private"
     service = "k8s"
   }
+  egress_gateway {
+    name    = "eks.egress.gateway"
+    service = "k8s"
+  }
+  internet_gateway {
+    name    = "eks.internet.gateway"
+    service = "k8s"
+  }
   cidrs = var.subnet_cidrs
+  azs   = data.aws_availability_zones.available_azs.names
 }
