5b805075a18d10e1084c6a32c6f299fd08672faf
Lee <lee.stratus@gmail.com>
2020-12-02 12:34:42 +0000

    Update main.tf

--- envs/main.tf
+++ envs/main.tf
@@ -2,7 +2,5 @@ module "test_vpc" {
   source            = "./../"
   name              = var.name
   vpc_cidr          = var.vpc_cidr
-  pub_subnet_cidrs  = var.pub_subnet_cidrs
-  priv_subnet_cidrs = var.priv_subnet_cidrs
   region            = var.region
 }
