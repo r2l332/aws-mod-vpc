7088a7b62051d97cfa79589ddeb302dbb055c2f7
Lee <lee.stratus@gmail.com>
2020-12-02 12:36:55 +0000

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,11 +1,7 @@
 variable "name" {}
 variable "vpc_cidr" {}
 variable "pub_subnet_cidrs" {
-  type = list
-  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 }
 variable "priv_subnet_cidrs" {
-  type = list
-  default = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"] 
 }
 variable "region" {}
