acfd924ba8f7e67a7574808f7faa67c0ee3b405a
Lee <lee.stratus@gmail.com>
2020-12-02 11:36:29 +0000

    Update terraform_test.go

--- test/terraform_test.go
+++ test/terraform_test.go
@@ -37,8 +37,8 @@ func testTerraformApply(t *testing.T, backendBucket, backendTable string) {
 			"name":   name,
 			"create_network": true,
 			"vpc_cidr": "10.0.0.0/16",
-			"pub_subnet_cidrs": ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"],
-			"priv_subnet_cidrs": ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"],
+			"pub_subnet_cidrs": "["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]",
+			"priv_subnet_cidrs": "["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]",
 		},
 // 		VarFiles: []string{"terraform.tfvars"},
 		EnvVars: map[string]string{
