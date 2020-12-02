5711deb3d9a7d17770845f2bb7a8f3b956568aef
Lee <lee.stratus@gmail.com>
2020-12-02 12:55:57 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,12 +9,10 @@ variable "vpc_cidr" {
 variable "pub_subnet_cidrs" {
   description = "List of public subnets to create"
   type        = list(string)
-  default     = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 }
 variable "priv_subnet_cidrs" {
   description = "List of private subnets to create"
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
 }
 variable "region" {
   description = "Where to deploy VPC"
