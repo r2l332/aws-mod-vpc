63403d78b48a4343453441b83b2c7fe796f4a22c
Lee <lee.stratus@gmail.com>
2020-12-02 11:51:30 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-  value       = module.vpc.eks_vpc_id
+  value       = module.vpc.vpc_id
   description = "Returns VPC id"
 }
 
 output "public_subnet" {
-  value       = module.vpc.eks_public_subnet
+  value       = module.vpc.public_subnet
   description = "Returns public subnet ids"
 }
 
 output "private_subnet" {
-  value       = module.vpc.eks_private_subnet
+  value       = module.vpc.private_subnet
   description = "Returns private subnet ids"
 }
 
