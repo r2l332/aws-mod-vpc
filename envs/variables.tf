b8156443c40ec40f9abe47b83eedf641864d8e7e
Lee <lee.stratus@gmail.com>
2020-12-02 12:42:22 +0000

    Update variables.tf

--- envs/variables.tf
+++ envs/variables.tf
@@ -9,5 +9,5 @@ variable "pub_subnet_cidrs" {
 variable "priv_subnet_cidrs" {
   description = "List of private subnets to create"
   type        = list(string)
-  default.    = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
 }
