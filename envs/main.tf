dccdd3b5923f19365ca3e6c28e2068a06edc269c
lee.jelley@contino.io <ljelley@lees-mbp.home>
2020-12-09 16:47:08 +0000

    Quick modification to use localstack

--- envs/main.tf
+++ envs/main.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  region                      = var.region
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+    endpoints {
+        ec2      = "http://localhost:4566"
+        dynamodb = "http://localhost:4566"
+        s3       = "http://localhost:4566"
+        sts      = "http://localhost:4566"
+  }
+}
+
 module "test_vpc" {
   source            = "./../"
   name              = var.name
