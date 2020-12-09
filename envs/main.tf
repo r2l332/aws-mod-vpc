b95755d43065eb14ea7979b9bc76fd4a9cceb79e
lee.jelley@contino.io <ljelley@lees-mbp.home>
2020-12-09 21:18:41 +0000

    formatted main file

--- envs/main.tf
+++ envs/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-    endpoints {
-        ec2      = "http://localhost:4566"
-        dynamodb = "http://localhost:4566"
-        s3       = "http://localhost:4566"
-        sts      = "http://localhost:4566"
+  endpoints {
+    ec2      = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    s3       = "http://localhost:4566"
+    sts      = "http://localhost:4566"
   }
 }
 
